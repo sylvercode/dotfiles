9365f8b249ec19b0e9d842b12b2fc5e8d2100ba9
sylver <sylver.dev@outlook.com>
2021-11-07 22:16:27 -0500

    correct PowerShell_profile.ps1 target path

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo ln -s /usr/share/zoneinfo/America/Montreal /etc/localtime
 
 pwsh ~/dotfiles/Powershell/InstallPoshModules.ps1
 mkdir -p ~/.config/powershell/
-cp ~/dotfiles/Powershell/Microsoft.PowerShell_profile.ps1 .config/powershell/
+cp ~/dotfiles/Powershell/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/
