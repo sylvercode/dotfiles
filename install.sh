a590aca2418457deb01e329c055c204208896e69
sylver <sylver.dev@outlook.com>
2021-11-07 22:12:43 -0500

    Make install executable

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/America/Montreal /etc/localtime
 
-su vscode -c "pwsh ~/dotfiles/Powershell/InstallPoshModules.ps1"
-su vscode -c "cp ~/dotfiles/Powershell/Microsoft.PowerShell_profile.ps1 .config/powershell/"
+pwsh ~/dotfiles/Powershell/InstallPoshModules.ps1
+mkdir -p ~/.config/powershell/
+cp ~/dotfiles/Powershell/Microsoft.PowerShell_profile.ps1 .config/powershell/
