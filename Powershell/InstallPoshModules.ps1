12738e9cbf677d7bf601128cb44de0820ef936c6
sylver <sylver.dev@outlook.com>
2021-11-08 02:09:10 +0000

    Locals config and Powershell theming

---
+++ Powershell/InstallPoshModules.ps1
@@ -0,0 +1,2 @@
+Install-Module oh-my-posh -Scope CurrentUser -AllowPrerelease -Force
+Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/America/Montreal /etc/localtime
+
+su vscode -c "pwsh ~/dotfiles/Powershell/InstallPoshModules.ps1"
+su vscode -c "cp ~/dotfiles/Powershell/Microsoft.PowerShell_profile.ps1 .config/powershell/"
