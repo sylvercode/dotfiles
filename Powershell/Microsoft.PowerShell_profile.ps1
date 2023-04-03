c3ce99a77185a58371fafef8bfb15c6c7df545e3
sylver <sylver.dev@outlook.com>
2023-04-03 01:46:53 +0000

    Add pnpm env var to pwsh profile

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 oh-my-posh init pwsh --config "~/.poshthemes/slimfat.omp.json" | Invoke-Expression
 Import-Module posh-git
+$env:PNPM_HOME="/home/$env:USER/.local/share/pnpm"
+$env:PATH="$env:PNPM_HOME`:$env:PATH"
