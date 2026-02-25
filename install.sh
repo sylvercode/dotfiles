3d30d68d1d947c5ea6e9453eb92d66f8655d6b79
Sylvercode <987564+sylvercode@users.noreply.github.com>
2026-02-24 22:40:52 -0500

    Copy starship.toml to the config directory

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ scriptDirectory=$(dirname "$(readlink -f "$0")")
 pwsh $scriptDirectory/Powershell/InstallPoshModules.ps1
 mkdir -p ~/.config/powershell/
 cp $scriptDirectory/Powershell/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/
+
+cp $scriptDirectory/starship.toml ~/.config/starship.toml
