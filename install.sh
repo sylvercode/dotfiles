82c1fdc8c492005b076bb871b7fa7f07633bbe9e
Sylvercode <987564+sylvercode@users.noreply.github.com>
2026-02-24 23:02:35 -0500

    Update install.sh to install Starship
    Replaced unzip installation with Starship installation script.

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/America/Montreal /etc/localtime
-sudo apt-get install unzip
+sudo curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
 scriptDirectory=$(dirname "$(readlink -f "$0")")
 
-pwsh $scriptDirectory/Powershell/InstallPoshModules.ps1
 mkdir -p ~/.config/powershell/
 cp $scriptDirectory/Powershell/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/
 
