337940d97ffbdb0fa86220fb1cd47e131a792179
Sylvercode <sylver.dev@outlook.com>
2026-03-14 10:47:55 -0400

    add latest git and powershell

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
+
+# Timezone
 sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/America/Montreal /etc/localtime
+
+# Latest Git
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update -q
+sudo apt-get install -y git
+
+# Latest PowerShell
+curl -sL https://aka.ms/install-powershell.sh | sudo bash
+
+# Starship prompt
 sudo curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
 scriptDirectory=$(dirname "$(readlink -f "$0")")
@@ -10,4 +22,4 @@ cp $scriptDirectory/Powershell/Microsoft.PowerShell_profile.ps1 ~/.config/powers
 
 cp $scriptDirectory/starship.toml ~/.config/starship.toml
 
-pwsh -NoProfile -Command  Install-Module posh-git -Scope CurrentUser -Force
+pwsh -NoProfile -Command "Install-Module posh-git -Scope CurrentUser -Force"
